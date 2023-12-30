ce8f02d631cb71c160aa1bc4058843d80102ba68
xmatyas <adam.matyas2@gmail.com>
2023-12-30 15:21:29 +0100

    testing file upload

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,43 @@
-from flask import Flask
+# app.py
+from flask import Flask, render_template, request, redirect, url_for
+import os
 
 app = Flask(__name__)
 
-@app.get("/")
-def home():
-    return "Hello, World!"
+UPLOAD_FOLDER = 'downloads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return redirect(request.url)
+
+    if file and allowed_file(file.filename):
+        filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filename)
+        return redirect(url_for('index'))
+
+    return redirect(request.url)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
